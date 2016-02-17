c93e1574847333ff3a5afae0254f3faeec89b469
ShaliovArtiom <shaliov.artiom@mail.ru>
2016-02-17 17:15:31 +0300

    background

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import BackGround.SimpleFrame;
+
+import javax.swing.*;
+/**
+ * Created by TyZiK on 17.02.2016.
+ */
+public class Main {
+    public static void main(String[] argc)
+    {
+        SimpleFrame frame = new SimpleFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
