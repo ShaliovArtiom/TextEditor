468ff1b613f325cc76862dd2d176fa8a4a91f427
ShaliovArtiom <shaliov.artiom@mail.ru>
2016-02-24 22:23:08 +0300

    panel

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import Menu.MenuFrame;
 
 import javax.swing.*;
+
 /**
  * Created by TyZiK on 17.02.2016.
  */
 public class Main {
     public static void main(String[] argc)
     {
-        MenuFrame frame = new MenuFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        MenuFrame menu = new MenuFrame();
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menu.setVisible(true);
 
 
     }
+
+
 }
--- src/Menu/MenuFrame.java
+++ src/Menu/MenuFrame.java
@@ -12,12 +12,97 @@ import java.awt.event.MouseAdapter;
 
 public class MenuFrame extends JFrame {
     public MenuFrame() {
+        //menu
 
-        toolBar = new JPanel();
+        setTitle("TextEditor");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
+        JTextPane jtp = new JTextPane();
+        add(jtp, BorderLayout.CENTER);
+
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newItem = fileMenu.add(new TestAction("New"));
+        JMenuItem openItem = fileMenu.add(new TestAction("Open"));
+        fileMenu.addSeparator();
+
+        saveAction = new TestAction("Save");
+        JMenuItem saveItem = fileMenu.add(saveAction);
+
+        saveAsAction = new TestAction("Save As");
+        JMenuItem saveAsItem = fileMenu.add(saveAsAction);
+        fileMenu.addSeparator(); // Разделитель
+
+        fileMenu.add(new AbstractAction("Exit") {
+
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+
+            }
+
+
+        });
+
+        readonlyItem = new JCheckBoxMenuItem("Read-only");
+        readonlyItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent event) {
+                boolean saveOk = !readonlyItem.isSelected();
+                saveAction.setEnabled(saveOk);
+                saveAsAction.setEnabled(saveOk);
+
+            }
+        });
+
+
+        Action cutAction = new TestAction("Cut");
+        Action copyAction = new TestAction("Copy");
+        Action pasteAction = new TestAction("Paste");
+
+        JMenu editMenu = new JMenu("Edit");
+        editMenu.add(cutAction);
+        editMenu.add(copyAction);
+        editMenu.add(pasteAction);
+
+
+        JMenu optionMenu = new JMenu("Options");
+
+        optionMenu.add(readonlyItem);
+
+        editMenu.addSeparator();
+        editMenu.add(optionMenu);
+
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.setMnemonic('H');
+
+        JMenuItem indexItem = new JMenu("Index");
+        indexItem.setMnemonic('I');
+        helpMenu.add(indexItem);
+
+        JMenuItem aboutItem = new JMenu("About");
+        indexItem.setMnemonic('A');
+        helpMenu.add(aboutItem);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
+        menuBar.add(helpMenu);
+
+        // Всплывающее меню(от правой кнопки мыши)
+        popup = new JPopupMenu();
+        popup.add(cutAction);
+        popup.add(copyAction);
+        popup.add(pasteAction);
+
+        jtp.setComponentPopupMenu(popup);
+        jtp.addMouseListener(new MouseAdapter() {});
+
+        //toolbar
 
         Action copyActhion = new AbstractAction("Copy", new ImageIcon("copy.jpeg")) {
             public void actionPerformed(ActionEvent e) {
+
             }
         };
 
@@ -25,6 +110,7 @@ public class MenuFrame extends JFrame {
 
         Action cutActhion = new AbstractAction("Cut", new ImageIcon("cut.jpeg")) {
             public void actionPerformed(ActionEvent e) {
+
             }
         };
 
@@ -37,12 +123,9 @@ public class MenuFrame extends JFrame {
 
         pasteActhion.putValue(Action.SHORT_DESCRIPTION, "Pastte your text");
 
-        int w = getSize().width;
-        int h = getSize().height;
         String[] sizeOfWord = new String[]{"8" , "9" , "10" , "11" ,"12" , "13" ,"14" , "16" , "18" , "20" , "22" ,
                 "24" , "26" , "28" , "36", "48" , "72"};
         JComboBox comboSize = new JComboBox(sizeOfWord);
-
         comboSize.setSelectedIndex(1);
 
         String[] stillOfWord = new String[]{"Still 1","Still2" };
@@ -80,117 +163,11 @@ public class MenuFrame extends JFrame {
         bar.add(underlinedActhion);
         add(bar, BorderLayout.NORTH);
 
-
-        JMenu menu1 = new JMenu("Bar");
-        JMenuBar menuBar1 = new JMenuBar();
-        menuBar1.add(menu1);
-        setJMenuBar(menuBar1);
-
-        //end Tool Bar
-
-
-        setTitle("TextEditor");
-        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem newItem = fileMenu.add(new TestAction("New"));
-        JMenuItem openItem = fileMenu.add(new TestAction("Open"));
-        fileMenu.addSeparator();
-
-        saveAction = new TestAction("Save");
-        JMenuItem saveItem = fileMenu.add(saveAction);
-
-        saveAsAction = new TestAction("Save As");
-        JMenuItem saveAsItem = fileMenu.add(saveAsAction);
-        fileMenu.addSeparator(); // Разделитель
-
-        fileMenu.add(new AbstractAction("Exit") {
-
-            public void actionPerformed(ActionEvent event) {
-                System.exit(0);
-
-            }
-        });
-
-        readonlyItem = new JCheckBoxMenuItem("Read-only");
-        readonlyItem.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent event) {
-                boolean saveOk = !readonlyItem.isSelected();
-                saveAction.setEnabled(saveOk);
-                saveAsAction.setEnabled(saveOk);
-
-            }
-        });
-
-        ButtonGroup group = new ButtonGroup();
-
-        JRadioButtonMenuItem insertItem = new JRadioButtonMenuItem("Insert");
-        insertItem.setSelected(true);
-
-        JRadioButtonMenuItem overtypeItem = new JRadioButtonMenuItem("Overtype");
-
-        group.add(insertItem);
-        group.add(overtypeItem);
-
-        Action cutAction = new TestAction("Cut");
-        Action copyAction = new TestAction("Copy");
-        Action pasteAction = new TestAction("Paste");
-
-        JMenu editMenu = new JMenu("Edit");
-        editMenu.add(cutAction);
-        editMenu.add(copyAction);
-        editMenu.add(pasteAction);
-
-
-        JMenu optionMenu = new JMenu("Options");
-
-        optionMenu.add(readonlyItem);
-        optionMenu.addSeparator();
-        optionMenu.add(insertItem);
-        optionMenu.add(overtypeItem);
-
-        editMenu.addSeparator();
-        editMenu.add(optionMenu);
-
-        JMenu helpMenu = new JMenu("Help");
-        helpMenu.setMnemonic('H');
-
-        JMenuItem indexItem = new JMenu("Index");
-        indexItem.setMnemonic('I');
-        helpMenu.add(indexItem);
-
-        JMenuItem aboutItem = new JMenu("About");
-        indexItem.setMnemonic('A');
-        helpMenu.add(aboutItem);
-
-        JMenuBar menuBar = new JMenuBar();
-        setJMenuBar(menuBar);
-
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(helpMenu);
-
-
-        // Всплывающее меню(от правой кнопки мыши)
-        popup = new JPopupMenu();
-        popup.add(cutAction);
-        popup.add(copyAction);
-        popup.add(pasteAction);
-
-        JPanel panel = new JPanel();
-        panel.setComponentPopupMenu(popup);
-        add(panel);
-
-        panel.addMouseListener(new MouseAdapter() {});
-
-
     }
     private  Action saveAction;
     private  Action saveAsAction;
     private  JCheckBoxMenuItem readonlyItem;
     private  JPopupMenu popup;
-    private JPanel toolBar;
     private JComboBox face;
 
     public static final int DEFAULT_HEIGHT = 600;
@@ -198,14 +175,15 @@ public class MenuFrame extends JFrame {
 
 }
 
-class TestAction extends AbstractAction
-{
-    public TestAction(String name) {super(name);}
+class TestAction extends AbstractAction {
+    public TestAction(String name) {
+        super(name);
+    }
 
-    public void actionPerformed(ActionEvent event)
-    {
+    public void actionPerformed(ActionEvent event) {
         System.out.println(getValue(Action.NAME) + " selected.");
 
     }
+
 
 }
